759c8a4db12a725f3e6f2fe39ac8ffe884094e20
Collin Alpert <collinalpert@gmail.com>
2020-08-01 20:30:16 +0200

    Added symbol field to currency.

--- src/main/java/com/github/collinalpert/teeprapi/entities/Currency.java
+++ src/main/java/com/github/collinalpert/teeprapi/entities/Currency.java
@@ -6,8 +6,13 @@ import com.github.collinalpert.java2db.entities.BaseEntity;
 @TableName("currency")
 public class Currency extends BaseEntity {
 	private String name;
+	private String symbol;
 
 	public String getName() {
 		return name;
+	}
+
+	public String getSymbol() {
+		return symbol;
 	}
 }
